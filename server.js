6e0d19feb057a799414453a687a8b7543ed702de
Gabriel <GABKASZ47@edu.gdansk.pl>
2020-12-01 22:18:18 +0100

    Connect to the database (globally?)

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ const app = express();
 let env = process.env.NODE_ENV || 'dev';
 const config = require('./environment')[env];
 const db = require('./database');
+db.connectToDatabase(config);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+const characterRoutes = require('./routes/character.routes');
+
+app.use('/', characterRoutes);
+
 app.get("/", async (req, res) => {
-    let dbResponse = await db.connectToDatabase(config);
-    if (!dbResponse) return res.send('Error: Cannot connect to the database!');
     res.send("Welcome to Rick & Morty Tournament API!");
 });
 
