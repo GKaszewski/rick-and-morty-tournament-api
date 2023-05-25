a49f85ee84d64c3f7e28ab162eab11841058401f
Gabriel Kaszewski <gabrielkaszewski@gmail.com>
2023-05-25 20:22:00 +0100

    Update environment.js

---
+++ environment.js
@@ -0,0 +1,18 @@
+module.exports = {
+	dev: {
+		database: {
+			uri: "mongodb://",
+		},
+		server: {
+			port: 3000,
+		},
+	},
+	prod: {
+		database: {
+			uri: "mongodb://",
+		},
+		server: {
+			port: 3000,
+		},
+	},
+};
