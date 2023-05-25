d6c4457dd3716c93c3ab97464477730c93ad9179
Gabriel Kaszewski <gabrielkaszewski@gmail.com>
2023-05-25 21:13:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const path = require('path');
 
 let env = process.env.NODE_ENV || 'dev';
 const config = require('./environment')[env];
 const db = require('./database');
 db.connectToDatabase(config);
 
+app.use(express.static('public/'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
@@ -19,7 +21,7 @@ const characterRoutes = require('./routes/character.routes');
 app.use('/', characterRoutes);
 
 app.get("/", async (req, res) => {
-    res.send("Welcome to Rick & Morty Tournament API!");
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
 app.listen(config.server.port, ()=>console.log("listening"));
